844dafa95c929380cbbb556e0b58bc947958ba3e
Kavindu Harshitha <kavindu.harshitha97@gmail.com>
2024-12-07 12:11:14 +1030

    updated message printing

--- apexms/cli/init.py
+++ apexms/cli/init.py
@@ -2,19 +2,16 @@ import os
 import re
 import yaml
 from .tool import tool, click
-from apexms.handlers.project.init import init as init_handler
+from apexms.handlers.project import init as init_project
 
 
 @tool.command(name="init", help="Initialize a new project")
 @click.argument("project_name", required=True)
 @click.option("--name", "-n", help="Name of the project")
 @click.option("--description", "-d", help="Description of the project")
-@click.option("--path", "-p", help="Path to the project")
+@click.option("--path", "-p", help="Path to the project", default=os.getcwd())
 def init(project_name, name, description, path):
-    msgs = init_handler(project_name, name, description, path)
-
-    for msg in msgs:
-        click.echo(msg)
+    init_project(project_name, name, description, path, click.echo)
 
 
 tool.add_command(init)
