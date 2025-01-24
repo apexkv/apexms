50d6cdef53ef85be85a2e96865e7f5787aad3c64
Kavindu Harshitha <kavindu.harshitha97@gmail.com>
2025-01-24 22:13:36 +1030

    configured file base routing and tailwindcss

---
+++ frontend/src/components/NotFound.tsx
@@ -0,0 +1,9 @@
+function NotFound() {
+    return (
+        <div className="w-full h-screen flex items-center justify-center">
+            <h1 className="text-6xl">404 Invalid Page</h1>
+        </div>
+    );
+}
+
+export default NotFound;
--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,9 +1,18 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import Pages from 'vite-plugin-pages'
+import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-    plugins: [react()],
+    plugins: [
+        react(),
+        tailwindcss(),
+        Pages({
+            dirs: 'src/pages',
+            extensions: ['tsx'],
+        })
+    ],
     preview: {
         port: 4186,
     },
